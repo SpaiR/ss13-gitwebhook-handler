761b1fd8b5c75684441c2824935b4ee2152db9a5
SpaiR <klimbetoo@gmail.com>
2017-11-11 22:24:56 +0300

    ChangelogService refactor Some of private functionality move into new package-private ChangelogParser class

---
+++ src/main/java/io/github/spair/services/changelog/ChangelogParser.java
@@ -0,0 +1,139 @@
+package io.github.spair.services.changelog;
+
+import io.github.spair.services.changelog.entities.Changelog;
+import io.github.spair.services.changelog.entities.ChangelogRow;
+import io.github.spair.services.config.ConfigService;
+import io.github.spair.services.git.entities.PullRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Service
+class ChangelogParser {
+
+    private final ConfigService configService;
+
+    private static final Pattern CHANGELOG_TEXT_PATTERN = Pattern.compile(":cl:((?:.|\\n|\\r)*+)|\uD83C\uDD91((?:.|\\n|\\r)*+)");
+    private static final Pattern AUTHOR_BEFORE_CHANGES_PATTERN = Pattern.compile(".*");
+    private static final Pattern CHANGELOG_ROW_WITH_CLASS_PATTERN = Pattern.compile("-\\s(\\w+)(\\[link])?:\\s(.*)");
+
+    @Autowired
+    public ChangelogParser(ConfigService configService) {
+        this.configService = configService;
+    }
+
+    Changelog createFromPullRequest(PullRequest pullRequest) {
+        Changelog changelog = null;
+        String changelogText = findChangelogText(pullRequest.getBody());
+
+        if (changelogText != null) {
+            changelog = parseChangelog(changelogText);
+            prepareChangelog(changelog, pullRequest);
+        }
+
+        return changelog;
+    }
+
+    private String findChangelogText(String prBody) {
+        String changelogText = null;
+
+        String changelogWithoutComments = prBody.replaceAll("(?s)<!--.*?-->", "");
+        Matcher matcher = CHANGELOG_TEXT_PATTERN.matcher(changelogWithoutComments);
+
+        if (matcher.find()) {
+            changelogText = matcher.group(1) != null ? matcher.group(1) : matcher.group(2);  // 1 - :cl:, 2 - 🆑
+        }
+
+        return changelogText;
+    }
+
+    private Changelog parseChangelog(String changelogText) {
+        Changelog changelog = new Changelog();
+
+        changelog.setAuthor(parseAuthor(changelogText));
+        changelog.setChangelogRows(parseChangelogRows(changelogText));
+
+        return changelog;
+    }
+
+    private String parseAuthor(String changelogText) {
+        String author = "";
+
+        Matcher matcher = AUTHOR_BEFORE_CHANGES_PATTERN.matcher(changelogText);
+
+        if (matcher.find()) {
+            author = matcher.group().trim();
+        }
+
+        return author.length() > 0 ? author : null;
+    }
+
+    private List<ChangelogRow> parseChangelogRows(String changelogText) {
+        List<ChangelogRow> changelogRows = new ArrayList<>();
+
+        try (Scanner scanner = new Scanner(changelogText)) {
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+
+                Matcher matcher = CHANGELOG_ROW_WITH_CLASS_PATTERN.matcher(line);
+
+                if (matcher.find()) {
+                    ChangelogRow changelogRow = new ChangelogRow();
+
+                    changelogRow.setClassName(matcher.group(1));
+                    changelogRow.setHasLink(matcher.group(2) != null);
+                    changelogRow.setChanges(matcher.group(3));
+
+                    changelogRows.add(changelogRow);
+                }
+            }
+        }
+
+        return changelogRows;
+    }
+
+    private void prepareChangelog(Changelog changelog, PullRequest pullRequest) {
+        ensureAuthorExist(changelog, pullRequest.getAuthor());
+
+        changelog.getChangelogRows().forEach(changelogRow -> {
+            String changes = changelogRow.getChanges().trim();
+            StringBuilder sb = new StringBuilder();
+
+            capitalizeFirstLetter(sb, changes);
+            ensureDotEnd(sb);
+            addPullRequestLink(changelogRow.isHasLink(), sb, pullRequest.getLink());
+
+            changelogRow.setChanges(sb.toString());
+        });
+    }
+
+    private void ensureAuthorExist(Changelog changelog, String authorName) {
+        if (changelog.getAuthor() == null) {
+            changelog.setAuthor(authorName);
+        }
+    }
+
+    private void capitalizeFirstLetter(StringBuilder sb, String changes) {
+        sb.append(changes.substring(0, 1).toUpperCase()).append(changes.substring(1));
+    }
+
+    private void ensureDotEnd(StringBuilder sb) {
+        String lastChar = sb.substring(sb.length() - 1);
+
+        if (!lastChar.equals(".") && !lastChar.equals("?") && !lastChar.equals("!")) {
+            sb.append(".");
+        }
+    }
+
+    private void addPullRequestLink(boolean hasLink, StringBuilder sb, String prLink) {
+        if (hasLink) {
+            String moreText = configService.getConfig().getChangelogConfig().getHtml().getMoreText();
+            sb.append(" <a href=\"").append(prLink).append("\">- ").append(moreText).append(" -</a>");
+        }
+    }
+}
--- src/main/java/io/github/spair/services/changelog/ChangelogService.java
+++ src/main/java/io/github/spair/services/changelog/ChangelogService.java
@@ -1,7 +1,6 @@
 package io.github.spair.services.changelog;
 
 import io.github.spair.services.changelog.entities.Changelog;
-import io.github.spair.services.changelog.entities.ChangelogRow;
 import io.github.spair.services.changelog.entities.ChangelogValidationStatus;
 import io.github.spair.services.config.ConfigService;
 import io.github.spair.services.git.GitHubService;
@@ -13,9 +12,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @Service
 public class ChangelogService {
@@ -24,27 +20,25 @@ public class ChangelogService {
     private final HtmlChangelogGenerator htmlChangelogGenerator;
     private final ChangelogValidator changelogValidator;
     private final ConfigService configService;
+    private final ChangelogParser changelogParser;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ChangelogService.class);
-
-    private static final Pattern CHANGELOG_TEXT_PATTERN = Pattern.compile(":cl:((?:.|\\n|\\r)*+)|\uD83C\uDD91((?:.|\\n|\\r)*+)");
-    private static final Pattern AUTHOR_BEFORE_CHANGES_PATTERN = Pattern.compile(".*");
-    private static final Pattern CHANGELOG_ROW_WITH_CLASS_PATTERN = Pattern.compile("-\\s(\\w+)(\\[link])?:\\s(.*)");
 
     @Autowired
     public ChangelogService(GitHubService gitHubService,
                             HtmlChangelogGenerator htmlChangelogGenerator,
                             ChangelogValidator changelogValidator,
-                            ConfigService configService)
+                            ConfigService configService, ChangelogParser changelogParser)
     {
         this.gitHubService = gitHubService;
         this.htmlChangelogGenerator = htmlChangelogGenerator;
         this.changelogValidator = changelogValidator;
         this.configService = configService;
+        this.changelogParser = changelogParser;
     }
 
     public void generateAndUpdate(PullRequest pullRequest) {
-        Changelog changelog = findAndCreate(pullRequest);
+        Changelog changelog = changelogParser.createFromPullRequest(pullRequest);
 
         if (changelog != null) {
             String changelogPath = configService.getConfig().getChangelogConfig().getPathToChangelog();
@@ -60,7 +54,7 @@ public class ChangelogService {
 
     public void validate(PullRequest pullRequest) {
         String invalidChangelogLabel = configService.getConfig().getGitHubConfig().getLabels().getInvalidChangelog();
-        Changelog changelog = findAndCreate(pullRequest);
+        Changelog changelog = changelogParser.createFromPullRequest(pullRequest);
 
         boolean isValid = true;
         boolean hasInvalidLabel = gitHubService.getIssueLabels(pullRequest.getNumber()).contains(invalidChangelogLabel);
@@ -86,7 +80,7 @@ public class ChangelogService {
     }
 
     public List<String> getChangelogClassesList(PullRequest pullRequest) {
-        Changelog changelog = findAndCreate(pullRequest);
+        Changelog changelog = changelogParser.createFromPullRequest(pullRequest);
 
         if (changelog != null && changelog.getChangelogRows().size() > 0) {
             List<String> changelogClasses = new ArrayList<>();
@@ -95,115 +89,5 @@ public class ChangelogService {
         }
 
         return new ArrayList<>();
-    }
-
-    private Changelog findAndCreate(PullRequest pullRequest) {
-        Changelog changelog = null;
-        String changelogText = findChangelogText(pullRequest.getBody());
-
-        if (changelogText != null) {
-            changelog = parseChangelog(changelogText);
-            prepareChangelog(changelog, pullRequest);
-        }
-
-        return changelog;
-    }
-
-    private String findChangelogText(String prBody) {
-        String changelogText = null;
-
-        String changelogWithoutComments = prBody.replaceAll("(?s)<!--.*?-->", "");
-        Matcher matcher = CHANGELOG_TEXT_PATTERN.matcher(changelogWithoutComments);
-
-        if (matcher.find()) {
-            changelogText = matcher.group(1) != null ? matcher.group(1) : matcher.group(2);  // 1 - :cl:, 2 - 🆑
-        }
-
-        return changelogText;
-    }
-
-    private Changelog parseChangelog(String changelogText) {
-        Changelog changelog = new Changelog();
-
-        changelog.setAuthor(parseAuthor(changelogText));
-        changelog.setChangelogRows(parseChangelogRows(changelogText));
-
-        return changelog;
-    }
-
-    private String parseAuthor(String changelogText) {
-        String author = "";
-
-        Matcher matcher = AUTHOR_BEFORE_CHANGES_PATTERN.matcher(changelogText);
-
-        if (matcher.find()) {
-            author = matcher.group().trim();
-        }
-
-        return author.length() > 0 ? author : null;
-    }
-
-    private List<ChangelogRow> parseChangelogRows(String changelogText) {
-        List<ChangelogRow> changelogRows = new ArrayList<>();
-
-        try (Scanner scanner = new Scanner(changelogText)) {
-            while (scanner.hasNext()) {
-                String line = scanner.nextLine();
-
-                Matcher matcher = CHANGELOG_ROW_WITH_CLASS_PATTERN.matcher(line);
-
-                if (matcher.find()) {
-                    ChangelogRow changelogRow = new ChangelogRow();
-
-                    changelogRow.setClassName(matcher.group(1));
-                    changelogRow.setHasLink(matcher.group(2) != null);
-                    changelogRow.setChanges(matcher.group(3));
-
-                    changelogRows.add(changelogRow);
-                }
-            }
-        }
-
-        return changelogRows;
-    }
-
-    private void prepareChangelog(Changelog changelog, PullRequest pullRequest) {
-        ensureAuthorExist(changelog, pullRequest.getAuthor());
-
-        changelog.getChangelogRows().forEach(changelogRow -> {
-            String changes = changelogRow.getChanges().trim();
-            StringBuilder sb = new StringBuilder();
-
-            capitalizeFirstLetter(sb, changes);
-            ensureDotEnd(sb);
-            addPullRequestLink(changelogRow.isHasLink(), sb, pullRequest.getLink());
-
-            changelogRow.setChanges(sb.toString());
-        });
-    }
-
-    private void ensureAuthorExist(Changelog changelog, String authorName) {
-        if (changelog.getAuthor() == null) {
-            changelog.setAuthor(authorName);
-        }
-    }
-
-    private void capitalizeFirstLetter(StringBuilder sb, String changes) {
-        sb.append(changes.substring(0, 1).toUpperCase()).append(changes.substring(1));
-    }
-
-    private void ensureDotEnd(StringBuilder sb) {
-        String lastChar = sb.substring(sb.length() - 1);
-
-        if (!lastChar.equals(".") && !lastChar.equals("?") && !lastChar.equals("!")) {
-            sb.append(".");
-        }
-    }
-
-    private void addPullRequestLink(boolean hasLink, StringBuilder sb, String prLink) {
-        if (hasLink) {
-            String moreText = configService.getConfig().getChangelogConfig().getHtml().getMoreText();
-            sb.append(" <a href=\"").append(prLink).append("\">- ").append(moreText).append(" -</a>");
-        }
     }
 }
