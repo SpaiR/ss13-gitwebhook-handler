2243c8fc41a43a61447554b939a7b07cdb978c9a
SpaiR <klimbetoo@gmail.com>
2018-03-14 16:45:33 +0300

    State overflow description message

--- src/main/java/io/github/spair/service/dmi/StatesNumberAppender.java
+++ src/main/java/io/github/spair/service/dmi/StatesNumberAppender.java
@@ -1,15 +1,37 @@
 package io.github.spair.service.dmi;
 
+import io.github.spair.byond.dmi.Dmi;
 import io.github.spair.service.dmi.entities.ReportEntry;
 
 import static io.github.spair.service.dmi.ReportPrinter.NEW_LINE;
 
 public class StatesNumberAppender implements ReportAppender {
 
+    private static final String OVERFLOW_MESSAGE = " (overflow)";
+    private static final String OLD_DMI = "Old";
+    private static final String NEW_DMI = "New";
+
     @Override
     public void append(final StringBuilder sb, final ReportEntry reportEntry) {
         sb.append("States number:").append(NEW_LINE);
-        sb.append("- **Old DMI:** ").append(reportEntry.getOldStatesNumber()).append(NEW_LINE);
-        sb.append("- **New DMI:** ").append(reportEntry.getNewStatesNumber()).append(NEW_LINE);
+        appendNumber(sb, OLD_DMI, reportEntry.getOldStatesNumber());
+        appendNumber(sb, NEW_DMI, reportEntry.getNewStatesNumber());
+
+        if (reportEntry.isStateOverflow()) {
+            sb.append(NEW_LINE);
+            sb.append("*DMI file has limit to 512 states. States overflow is when this limit was overpassed."
+                    + " It's a **critical** problem which should be fixed.*");
+            sb.append(NEW_LINE);
+        }
+    }
+
+    private void appendNumber(final StringBuilder sb, final String dmiType, final int number) {
+        sb.append("- **").append(dmiType).append(" DMI:** ").append(number);
+
+        if (number > Dmi.MAX_STATES) {
+            sb.append(OVERFLOW_MESSAGE);
+        }
+
+        sb.append(NEW_LINE);
     }
 }
--- src/test/java/io/github/spair/service/dmi/StatesNumberAppenderTest.java
+++ src/test/java/io/github/spair/service/dmi/StatesNumberAppenderTest.java
@@ -23,4 +23,18 @@ public class StatesNumberAppenderTest {
         String expectedReport = ReadFileUtil.readFile("appenders-reports/states-number-report.txt");
         assertEquals(expectedReport, sb.toString());
     }
+
+    @Test
+    public void testAppendWithOverflow() {
+        StringBuilder sb = new StringBuilder();
+        ReportEntry reportEntry = new ReportEntry("icons/file.dmi");
+
+        reportEntry.setOldStatesNumber(570);
+        reportEntry.setNewStatesNumber(30);
+
+        appender.append(sb, reportEntry);
+
+        String expectedReport = ReadFileUtil.readFile("appenders-reports/states-number-overflow-report.txt");
+        assertEquals(expectedReport, sb.toString());
+    }
 }
