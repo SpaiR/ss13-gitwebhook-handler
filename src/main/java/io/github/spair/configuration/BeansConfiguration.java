07980a48337240ba8e098d635099056283074b00
SpaiR <klimbetoo@gmail.com>
2018-03-18 21:52:11 +0300

    Fix problem with headers handling
    Default HttpClient has difficulty understanding the latest RFC-compliant headers,
    so newer version should be enabled.

--- src/main/java/io/github/spair/configuration/BeansConfiguration.java
+++ src/main/java/io/github/spair/configuration/BeansConfiguration.java
@@ -1,8 +1,12 @@
 package io.github.spair.configuration;
 
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,7 +14,20 @@ import org.springframework.web.client.RestTemplate;
 public class BeansConfiguration {
 
     @Bean
-    public RestTemplate template(final RestTemplateBuilder builder) {
-        return builder.requestFactory(new HttpComponentsClientHttpRequestFactory()).build();
+    public ClientHttpRequestFactory clientHttpRequestFactory() {
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+
+        factory.setHttpClient(HttpClientBuilder.create()
+                .setDefaultRequestConfig(
+                        RequestConfig.custom().setCookieSpec(CookieSpecs.STANDARD).build()
+                ).build()
+        );
+
+        return factory;
+    }
+
+    @Bean
+    public RestTemplate template(final RestTemplateBuilder builder, final ClientHttpRequestFactory factory) {
+        return builder.requestFactory(factory).build();
     }
 }
