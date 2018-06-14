7380f75d22804d011016735c60fcf9c74ca2bf4b
SpaiR <klimbetoo@gmail.com>
2018-06-14 17:49:44 +0300

    Add current application version to the view

--- src/test/java/io/github/spair/controller/GeneralViewControllerTest.java
+++ src/test/java/io/github/spair/controller/GeneralViewControllerTest.java
@@ -1,20 +1,29 @@
 package io.github.spair.controller;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.ui.Model;
 
 import static org.junit.Assert.assertEquals;
 
 public class GeneralViewControllerTest {
 
+    private GeneralViewController controller;
+
+    @Before
+    public void setUp() {
+        controller = new GeneralViewController(Mockito.mock(BuildProperties.class));
+    }
+
     @Test
     public void testConfig() {
-        GeneralViewController controller = new GeneralViewController();
-        assertEquals("config", controller.config());
+        assertEquals("config", controller.config(Mockito.mock(Model.class)));
     }
 
     @Test
     public void testLogin() {
-        GeneralViewController controller = new GeneralViewController();
         assertEquals("login", controller.login());
     }
 }
